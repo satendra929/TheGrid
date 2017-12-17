3ec142d3638f7c9be73f878f2cf13b2899b7f748
satendra929 <satendra929@gmail.com>
2017-12-16 22:51:42 -0500

    Reaching Destination

--- thegrid.py
+++ thegrid.py
@@ -1,20 +1,30 @@
 from graphics import *
 from time import sleep
+import math
+from heapq import heappush, heappop
 
 def main() :
     win= GraphWin("TheGrid", 500, 500)
     win.setBackground('black')
+    draw_grid(win)
     startX=250
     startY=250
+    stpt= Point(startX,startY)
+    stcir=Circle(stpt,5)
+    stcir.setFill('red')
+    stcir.draw(win)
+    endX=100
+    endY=0
+    ept= Point(endX,endY)
+    ecir=Circle(ept,5)
+    ecir.setFill('blue')
+    ecir.draw(win)
     grid=[50,100,150,200,250,300,350,400,450]
     pt= Point(startX,startY)
     cir=Circle(pt,5)
     cir.setFill('green')
     cir.draw(win)
-    draw_grid(win)
-    for t in range(250) :
-        sleep(0.1)
-        cir.move(1,0)
+    find_path(cir,startX,startY,endX,endY,grid)
     win.getMouse()
     win.close()
 
@@ -46,6 +56,9 @@ def draw_grid(win) :
     l8=Line(Point(450,500),Point(450,0))
     l8.setFill('green')
     l8.draw(win)
+    lh0=Line(Point(0,0),Point(500,0))
+    lh0.setFill('green')
+    lh0.draw(win)
     lh=Line(Point(0,50),Point(500,50))
     lh.setFill('green')
     lh.draw(win)
@@ -76,16 +89,56 @@ def draw_grid(win) :
 
 def is_path(x,y,grid) :
     for c in grid :
-        dxc = currPoint.x - point1.x
-        dyc = currPoint.y - point1.y
-        dxl = point2.x - point1.x
-        dyl = point2.y - point1.y
-        cross = dxc * dyl - dyc * dxl
+        if x==c :
+            return True
+        elif y==c :
+            return True
+    return False
 
-        dxc = currPoint.x - point1.x
-        dyc = currPoint.y - point1.y
-        dxl = point2.x - point1.x
-        dyl = point2.y - point1.y
-        cross = dxc * dyl - dyc * dxl
+def heuristic(x,y,endX,endY) :
+    print endX,endY
+    a=math.fabs(x-endX)
+    b=math.fabs(y-endY)
+    return math.sqrt((a+b))
+
+def successors(x,y,grid) :
+    succ=[]
+    if is_path(x+1,y,grid) :
+        succ.append((x+1,y))
+    if is_path(x,y+1,grid) :
+        succ.append((x,y+1))
+    if is_path(x-1,y,grid) :
+        succ.append((x-1,y))
+    if is_path(x,y-1,grid) :
+        succ.append((x,y-1))
+    return succ
+
+def find_path(cir,startX,startY,endX,endY,grid) :
+    print endX,endY
+    fringe=[]
+    visited=[]
+    cX=startX
+    cY=startY
+    visited.append((cX,cY))
+    heu=heuristic(startX,startY,endX,endY)
+    heappush(fringe,(heu,(startX,startY)))
+    while fringe[0]!= IndexError :
+        state=heappop(fringe)
+        startX=state[1][0]
+        startY=state[1][1]
+        if startX==endX and startY==endY :
+            return True
+        else :
+            cir.move(startX-cX,startY-cY)
+            cX=startX
+            cY=startY
+            sleep(0.01)
+        for s in successors(startX,startY,grid) :
+            tempX=s[0]
+            tempY=s[1]
+            if (tempX,tempY) not in visited :
+                tempheu=heuristic(tempX,tempY,endX,endY)
+                heappush(fringe,(tempheu,(tempX,tempY)))
+                visited.append((tempX,tempY))
 
 main()
